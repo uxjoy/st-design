25839332aba7a9e08ac18a5e4dbc483bbedd2200
Joy210 <sohanoor404@gmail.com>
2026-05-24 13:28:52 +0600

    update

--- app/api/icons/route.js
+++
@@ -1,9 +0,0 @@
-// app/api/icons/route.js
-
-import { getIconsByCategory } from "@/lib/icons";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const icons = await getIconsByCategory();
-  return NextResponse.json(icons);
-}
