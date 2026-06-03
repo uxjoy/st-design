f8aeaf2056f134d9bb8fc8b918e925f0956ba39c
Joy210 <sohanoor404@gmail.com>
2026-06-03 17:37:32 +0600

    update

--- app/emails/lifestyle/page.jsx
+++ app/emails/lifestyle/page.jsx
@@ -1,10 +1,11 @@
 import EmailLayout from "@/components/email/EmailLayout";
 import EmailPageLayout from "../page";
+import lifestyle from "@/data/lifestyle";
 
 const LifeStyleEmailList = () => {
   return (
     <EmailPageLayout>
-      <EmailLayout serviceEmails={[]} />
+      <EmailLayout serviceEmails={lifestyle} />
     </EmailPageLayout>
   );
 };
---
+++ data/lifestyle.js
@@ -0,0 +1,179 @@
+const lifestyle = [
+  {
+    section: "Shop",
+    frames: [
+      {
+        name: "b2cShopOrderPlacementVendor",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6862-35942",
+        subject: "Shop Order Placement Vendor",
+      },
+      {
+        name: "b2cShopOrderPlacementCustomer",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6863-36087",
+        subject: "Shop Order Placement Customer",
+      },
+      {
+        name: "b2cShopMultiStoreOrderPlacementCustomer",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6863-36954",
+        subject: "Shop Multi Store Order Placement Customer",
+      },
+      {
+        name: "b2cShopOrderShippedCustomer",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6862-32004",
+        subject: "Shop Order Shipped Customer",
+      },
+      {
+        name: "b2cShopOrderDeliveredCustomer",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6862-31636",
+        subject: "Shop Order Delivered Customer",
+      },
+      {
+        name: "b2cShopOrderUserCancelled",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6863-37052",
+        subject: "Shop Order User Cancelled",
+      },
+      {
+        name: "b2cShopOrderSellerCancelled",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6863-37319",
+        subject: "Shop Order Seller Cancelled",
+      },
+      {
+        name: "b2cShopDonationOrderPurchaseConfirmation",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6863-38252",
+        subject: "Shop Donation Order Purchase Confirmation",
+      },
+      {
+        name: "b2cShopFullRefundInitiation",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6863-38348",
+        subject: "Shop Full Refund Initiation",
+      },
+      {
+        name: "b2cShopPartialRefundInitiation",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6863-38443",
+        subject: "Shop Partial Refund Initiation",
+      },
+    ],
+  },
+  {
+    section: "Voucher",
+    frames: [
+      {
+        name: "b2cVoucherPurchaseConfirmation",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6862-32224",
+        subject: "Voucher Purchase Confirmation",
+      },
+      {
+        name: "b2cVoucherPurchaseAsGift",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6862-32717",
+        subject: "Voucher Purchase As Gift",
+      },
+      {
+        name: "b2cVoucherGiftsReceived",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6862-32547",
+        subject: "Voucher Gifts Received",
+      },
+      {
+        name: "b2cVoucherPartnerNotification",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6862-33320",
+        subject: "Voucher Partner Notification",
+      },
+      {
+        name: "b2cVoucherOtp",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6862-33639",
+        subject: "Voucher Otp",
+      },
+      {
+        name: "b2cVoucherOtpInGiftReceiver",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6862-33838",
+        subject: "Voucher Otp In Gift Receiver",
+      },
+      {
+        name: "b2cVoucherRedemptionBuyer",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6862-34126",
+        subject: "Voucher Redemption Buyer",
+      },
+      {
+        name: "b2cVoucherRedemptionGiftReceiver",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6862-34671",
+        subject: "Voucher Redemption Gift Receiver",
+      },
+    ],
+  },
+  {
+    section: "Mobile Recharge",
+    frames: [
+      {
+        name: "b2cMobileRechargeConfirmation",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6862-32891",
+        subject: "Mobile Recharge Confirmation",
+      },
+      {
+        name: "b2cMobileRechargeRefundInitiation",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6863-38535",
+        subject: "Mobile Recharge Refund Initiation",
+      },
+    ],
+  },
+  {
+    section: "Utility",
+    frames: [
+      {
+        name: "b2cUtilityPaymentConfirmation",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6862-34772",
+        subject: "Utility Payment Confirmation",
+      },
+      {
+        name: "b2cUtilityBillPaymentRefundInitiation",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6863-38627",
+        subject: "Utility Bill Payment Refund Initiation",
+      },
+    ],
+  },
+  {
+    section: "Partner",
+    frames: [
+      {
+        name: "stPartnerActivationEmail",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6862-35594",
+        subject: "Partner Activation Email",
+      },
+      {
+        name: "stPartnerForgetPassword",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6862-35171",
+        subject: "Forgot Password?",
+      },
+      {
+        name: "b2cVendorPanelGeneralPurposeTemplate",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6863-36474",
+        subject: "Vendor Panel General Purpose Template",
+      },
+    ],
+  },
+  {
+    section: "Attachment & Others",
+    frames: [
+      {
+        name: "b2cLifestyleSalesSummary",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6863-37657",
+        subject: "Lifestyle Sales Summary",
+      },
+      {
+        name: "b2cShopOrderPlacementVendorAttachment",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6864-42467",
+        subject: "Shop Order Placement Vendor Attachment",
+      },
+      {
+        name: "b2cShopOrderPlacementCustomerAttachment",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6864-42550",
+        subject: "Shop Order Placement Customer Attachment",
+      },
+      {
+        name: "b2cShopMultiStoreOrderPlacementCustomerAttachment",
+        link: "https://www.figma.com/design/DARvblwD3MEoBADqu8AKDa?node-id=6864-42672",
+        subject: "Shop Multi Store Order Placement Customer Attachment",
+      },
+    ],
+  },
+];
+
+export default lifestyle;
